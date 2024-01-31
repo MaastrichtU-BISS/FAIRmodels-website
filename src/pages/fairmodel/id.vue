c33c0dbb73dc0e0e2b7bdb5d124db58676204c25
eensander <50321538+eensander@users.noreply.github.com>
2024-01-31 14:33:27 +0100

    implement basic fairmodel version detail ui

--- src/pages/fairmodel/id.vue
+++ src/pages/fairmodel/id.vue
@@ -3,7 +3,7 @@ import { QTable, QTableProps, useQuasar } from 'quasar';
 import { Fairmodel, FairmodelVersion } from 'src/types';
 import { fairmodelApiService } from 'src/utils/fairmodel.api.service';
 import { fairmodelVersionApiService } from 'src/utils/fairmodelversion.api.service';
-import { onMounted, reactive, ref, watch } from 'vue';
+import { computed, onMounted, reactive, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 const $q = useQuasar()
@@ -30,14 +30,18 @@ onMounted(async () => {
   tableRef.value?.requestServerInteraction()
 })
 
+const getMetadataURL = (id: string) => {
+  return `https://cedar.metadatacenter.org/instances/edit/https://repo.metadatacenter.org/template-instances/${id}`
+}
+
 const table = reactive({
   columns: [
     {name: 'id', label: 'Id', field: (row: FairmodelVersion) => row.id, align: 'left' },
     {name: 'version', label: 'Name', field: (row: FairmodelVersion) => row.version, align: 'left' },
     {name: 'update_desc', label: 'Update Description', field: (row: FairmodelVersion) => row.update_description, align: 'left' },
-    {name: 'metadata_id', label: 'Cedar Metadata ID', field: (row: FairmodelVersion) => row.metadata_id === '' ? 'false' : row.metadata_id, align: 'left' },
-    {name: 'onnx', label: 'ONNX representation', field: (row: FairmodelVersion) => row.has_onnx, align: 'left' },
-    {name: 'actions', label: 'Actions', align: 'right' },
+    {name: 'metadata', label: 'Cedar Metadata ID', field: (row: FairmodelVersion) => row.metadata_id === '' ? 'false' : row.metadata_id, align: 'center' },
+    {name: 'model', label: 'Model Representation', field: (row: FairmodelVersion) => row.has_model, align: 'center' },
+    {name: 'actions', label: 'Actions', align: 'center' },
   ] as QTableProps['columns'],
   rows: [],
   loading: true,
@@ -58,19 +62,19 @@ const tableOnRequest = async () => {
   }
 }
 
-const createFairmodelVersionDialog = ref(false);
+// DIALOG: create fairmodel version
+const dialogCreateFairmodelVersion = ref(false);
 const newFairmodelVersionDefault = {
   update_description: '',
   update_type: '',
 }
-
 const newFairmodelVersionData = ref({...newFairmodelVersionDefault});
 
 const createFairmodelVersion = async () => {
   const create = await fairmodelVersionApiService.create(fairmodel.value!.id, newFairmodelVersionData.value)
   if (create.status == 200) {
     $q.notify({type: 'positive', position: 'top-right', message: create.data.message})
-    createFairmodelVersionDialog.value = false;
+    dialogCreateFairmodelVersion.value = false;
     newFairmodelVersionData.value = {...newFairmodelVersionDefault}
     tableRef.value?.requestServerInteraction()
   } else {
@@ -78,6 +82,106 @@ const createFairmodelVersion = async () => {
   }
 }
 
+// DIALOG: set metadata
+const setMetadataId = ref<FairmodelVersion['id']>()
+const dialogSetMetadata = computed({
+  get: () => setMetadataId.value !== undefined,
+  set: (state: boolean) => setMetadataId.value = undefined // Always assume false when set directly
+})
+const actionSetMetadata = (version: FairmodelVersion) => {
+  setMetadataId.value = version.id;
+  Object.assign(setMetadataData, setMetadataDataDefault)
+}
+const setMetadataDataDefault = {
+  metadata_id: '',
+}
+const setMetadataData = reactive({...setMetadataDataDefault})
+const saveMetadata = async () => {
+  const update = await fairmodelVersionApiService.update(
+    fairmodel.value!.id,
+    setMetadataId.value!,
+    {metadata_id: setMetadataData.metadata_id}
+  )
+  if (update.status == 200) {
+    $q.notify({type: 'positive', message: update.data.message})
+    tableRef.value?.requestServerInteraction();
+    dialogSetMetadata.value = false;
+  } else {
+    $q.notify({type: 'warning', message: update.data.message})
+  }
+}
+
+// DIALOG: view metadata
+const viewMetadataObject = ref<FairmodelVersion>();
+const dialogViewMetadata = computed({
+  get: () => viewMetadataObject.value !== undefined,
+  set: (state: boolean) => viewMetadataObject.value = undefined // Always assume false when set directly
+})
+const actionViewMetadata = (version: FairmodelVersion) => {
+  viewMetadataObject.value = {...version};
+}
+
+// DIALOG: set model file
+// const dialogSetModel1 = ref(false);
+const setModelDataDefault = {
+  model_type: 'ONNX' as 'ONNX' | 'PMML',
+  file: null as any | null
+};
+const setModelData = reactive({...setModelDataDefault})
+const setModelObject = ref<FairmodelVersion>();
+const dialogSetModel = computed({
+  get: () => setModelObject.value !== undefined,
+  set: (state: boolean) => setModelObject.value = undefined // Always assume false when set directly
+})
+const actionSetModel = (version: FairmodelVersion) => { 
+  setModelObject.value = {...version};
+  Object.assign(setModelData, setMetadataDataDefault);
+}
+const saveModelData = async () => {
+  const upload = await fairmodelVersionApiService.uploadModel(fairmodel.value!.id, setModelObject.value!.id, setModelData)
+  if (Math.floor(upload.status / 100) == 2) {
+    $q.notify({type: 'positive', message: upload.data.message})
+    tableRef.value?.requestServerInteraction();
+    dialogSetModel.value = false;
+  } else {
+    $q.notify({type: 'warning', message: upload.data.message})
+  }
+}
+
+// DIALOG: view model file
+const viewModelObject = ref<FairmodelVersion>();
+const dialogViewModel = computed({
+  get: () => viewModelObject.value !== undefined,
+  set: (state: boolean) => viewModelObject.value = undefined // Always assume false when set directly
+})
+const actionViewModel = (version: FairmodelVersion) => { 
+  viewModelObject.value = {...version};
+}
+const downloadLoading = ref(false);
+const downloadViewModel = async () => {
+  downloadLoading.value = true;
+  const down = await fairmodelVersionApiService.downloadModel(fairmodel.value!.id, viewModelObject.value!.id);
+  if (down.status == 200) {
+    const blob = new Blob([down.data], { type: down.headers['content-type'] });
+    const link = document.createElement('a');
+    link.download = `${fairmodel.value?.name.replace(/\s+/, '-')}-${viewModelObject.value!.version}.${viewModelObject.value!.model_type == 'ONNX' ? 'onnx' : 'pb'}`;
+    link.href = window.URL.createObjectURL(blob);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    downloadLoading.value = false;
+    // $q.notify({type: 'positive', message: 'Downloaded successfully'});
+    // dialogViewModel.value = false;
+  } else {
+    $q.notify({type: 'warning', message: down.data.message})
+  }
+
+}
+
+const actionLink = () => {
+  void(0)
+}
 </script>
 
 <template>
@@ -85,11 +189,11 @@ const createFairmodelVersion = async () => {
     <div class="row justify-center q-mt-xl">
       <div class="col-md-6">
         <h4 class="text-bold q-mt-md q-mb-lg">Fairmodel {{ fairmodel.name }}</h4>
-        <pre>{{ JSON.stringify(fairmodel) }}</pre>
+        <!-- <pre>{{ JSON.stringify(fairmodel) }}</pre> -->
 
-        <q-btn color="primary" label="Create new Fairmodel Version" @click="createFairmodelVersionDialog = true" />
+        <q-btn color="primary" label="New Version" @click="dialogCreateFairmodelVersion = true" />
 
-        <q-dialog v-model="createFairmodelVersionDialog">
+        <q-dialog v-model="dialogCreateFairmodelVersion">
           <q-card style="width: 64rem">
             <q-card-section>
               <div class="text-h5">Create new fairmodel version</div>
@@ -99,6 +203,7 @@ const createFairmodelVersion = async () => {
               >
                 <q-input
                   v-model="newFairmodelVersionData.update_description"
+                  filled
                   label="Update Description"
                 />
                 <q-radio v-model="newFairmodelVersionData.update_type" val="patch" label="Patch" />
@@ -110,7 +215,100 @@ const createFairmodelVersion = async () => {
             </q-card-section>
           </q-card>
         </q-dialog>
+        
+        <q-dialog v-model="dialogSetMetadata">
+          <q-card style="width: 64rem">
+            <q-card-section>
+              <div class="text-h5">Set metadata</div>
+              <q-form
+                @submit="saveMetadata"
+                class="q-gutter-md"
+              >
+                <q-input
+                  v-model="setMetadataData.metadata_id"
+                  filled
+                  label="CEDAR metadata ID"
+                />
 
+                <q-btn type="submit" color="primary" label="Submit" />
+              </q-form>
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+
+        <q-dialog v-model="dialogViewMetadata">
+          <q-card style="width: 64rem" v-if="viewMetadataObject">
+            <q-card-section>
+              <div class="text-h5">Metadata for {{ viewMetadataObject.version }}</div>
+              <div
+                class="q-gutter-md q-mt-lg"
+              >
+                <q-field
+                  label="CEDAR metadata ID"
+                  filled stack-label
+                  readonly
+                >
+                  <template v-slot:control>
+                    {{viewMetadataObject.metadata_id}}
+                  </template>
+                </q-field>
+
+                <q-field
+                  filled
+                  readonly
+                >
+                  <template v-slot:control>
+                    {{ getMetadataURL(viewMetadataObject.metadata_id) }}
+                  </template>
+                  <template v-slot:append>
+                    <q-btn
+                      :href="getMetadataURL(viewMetadataObject.metadata_id)"
+                      target="_blank"
+                      icon="open_in_new"
+                      ddcolor="primary"
+                      round
+                      flat
+                    />
+                  </template>
+                </q-field>
+              </div>
+
+              <div class="q-mt-md">Some more info here...</div>
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+        
+        <q-dialog v-model="dialogSetModel">
+          <q-card style="width: 64rem">
+            <q-card-section>
+              <div class="text-h5">Set model file</div>
+              <q-form
+                @submit="saveModelData"
+                class="q-gutter-md"
+              >
+                <q-select filled v-model="setModelData.model_type" :options="['ONNX', 'PMML']" label="Model type" />
+                <q-file filled v-model="setModelData.file" label="File"></q-file>
+
+                <q-btn type="submit" color="primary" label="Submit" />
+              </q-form>
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+
+        <q-dialog v-model="dialogViewModel">
+          <q-card style="width: 64rem">
+            <q-card-section>
+              <div class="text-h5">View model</div>
+              
+              <div>Here you can view and download the uploaded model for this version</div>.
+
+              <div>Model type: {{ viewModelObject?.model_type }}</div>
+            
+              <q-btn class="q-mt-md" :loading="downloadLoading" color="primary" label="Download" @click="downloadViewModel"></q-btn>
+            </q-card-section>
+          </q-card>
+        </q-dialog>
+        
         <div class="q-mt-lg row">
           <div class="col">
             <q-table
@@ -124,13 +322,25 @@ const createFairmodelVersion = async () => {
               @request="tableOnRequest"
               :pagination="{rowsPerPage: 10}"
             >
-              <!-- <template v-slot:body-cell-actions="props">
+              <template v-slot:body-cell-metadata="props">
                 <q-td :props="props">
-                  <q-btn size="sm" flat rounded icon="mode_edit" @click="actionEdit(props.row)"></q-btn>
-                  <q-btn size="sm" flat rounded icon="delete" @click="actionDelete(props.row)"></q-btn>
-                  <q-btn size="sm" flat rounded icon="visibility" @click="actionView(props.row)"></q-btn>
+                  <q-btn v-if="(props.row as FairmodelVersion).metadata_id === ''" color="grey-7" size="md" flat rounded icon="check_box_outline_blank" @click="actionSetMetadata(props.row)"></q-btn>
+                  <q-btn v-else color="green-10" flat rounded size="md" icon="check_box" @click="actionViewMetadata(props.row)" />
                 </q-td>
-              </template> -->
+              </template>
+              <template v-slot:body-cell-model="props">
+                <q-td :props="props">
+                  <q-btn v-if="!(props.row as FairmodelVersion).has_model" color="grey-7" size="md" flat rounded icon="check_box_outline_blank" @click="actionSetModel(props.row)"></q-btn>
+                  <q-btn v-else color="green-10" flat rounded size="md" icon="check_box" @click="actionViewModel(props.row)" />
+                </q-td>
+              </template>`
+              
+              <template v-slot:body-cell-actions="props">
+                <q-td :props="props">
+                  <!-- <q-btn size="sm" flat rounded icon="delete" @click="actionDelete(props.row)"></q-btn> -->
+                  <q-btn size="md" flat rounded icon="link" color="indigo-8" @click="actionLink(props.row)"></q-btn>
+                </q-td>
+              </template>
             </q-table>
           </div>
         </div>
