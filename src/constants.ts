862ca7cdf8b90552c93b53a4a910e8ffa894cd07
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-08-19 09:56:47 +0200

    Determine api backend location based on hostname

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
 
-
-export const API_BASE_URL = (import.meta.env.DEV) ? process.env.API_BASE_URL : 'https://api.fairmodels.org'
+var address = 'https://api.fairmodels.org';	// Production
+if (location.hostname === 'localhost') {
+    address = 'http://localhost:8000';	// Development
+}
+export const API_BASE_URL = address;
 
 export const EXECUTOR_BASE_URL = process.env.EXECUTOR_BASE_URL ?? `${API_BASE_URL}/executor`;
